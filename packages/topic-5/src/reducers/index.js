ceb37913c6f34f447e0108ddbab0d4d5cc34fcc2
Vasyl Kovalchuk <vasyl.kovalchuk2@eleks.com>
2018-07-24 22:27:57 +0300

    - add simple integration with redux and react navigation

---
+++ packages/topic-5/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import nav from "./nav";
+import auth from "./auth";
+
+const AppReducer = combineReducers({
+  nav,
+  auth,
+});
+
+export default AppReducer;
---
+++ packages/topic-5/src/reducers/nav.js
@@ -0,0 +1,39 @@
+import { combineReducers } from 'redux';
+import { NavigationActions } from 'react-navigation';
+
+import { RootNavigator } from '../navigators/AppNavigator';
+
+// Start with two routes: The Main screen, with the Login screen on top.
+const firstAction = RootNavigator.router.getActionForPathAndParams('Main');
+const tempNavState = RootNavigator.router.getStateForAction(firstAction);
+const secondAction = RootNavigator.router.getActionForPathAndParams('Login');
+const initialNavState = RootNavigator.router.getStateForAction(
+  secondAction,
+  tempNavState
+);
+
+function nav(state = initialNavState, action) {
+  let nextState;
+  switch (action.type) {
+    case 'Login':
+      nextState = RootNavigator.router.getStateForAction(
+        NavigationActions.back(),
+        state
+      );
+      break;
+    case 'Logout':
+      nextState = RootNavigator.router.getStateForAction(
+        NavigationActions.navigate({ routeName: 'Login' }),
+        state
+      );
+      break;
+    default:
+      nextState = RootNavigator.router.getStateForAction(action, state);
+      break;
+  }
+
+  // Simply return the original `state` if `nextState` is null or undefined.
+  return nextState || state;
+}
+
+export default nav;
