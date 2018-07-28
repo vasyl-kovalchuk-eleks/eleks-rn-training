9c328fb73d966262be9f3098fc60057359402213
andrew.stupchuk <andrii.stupchuk@eleks.com>
2018-07-28 18:08:24 +0300

    refactor push notificator

--- packages/topic-5/functions/pushNotifications.js
+++ packages/topic-5/functions/pushNotifications.js
@@ -3,33 +3,33 @@ const admin = require('firebase-admin');
 
 exports.sendPushNotification = functions.database.ref('/users/{uid}/pushToken')
   .onWrite((change, context) => {
-      // Only edit data when it is first created.
-      if (!change.before.exists()) {
-        const uid = context.params.uid;
-        const pushId = change.after.val();
+    // Only edit data when it is first created.
+    if (change.before.exists()) {
+      return null;
+    }
 
-        return admin.database().ref('users/' + uid).once('value', snap => {
-          const user = snap.val();
-          console.log('User ', user);
-          console.log('Push Token: ', pushId);
-          //Click the following link: https://rntrainingtest.page.link/welcome
-          const message = {
-            notification: {
-              title: 'Firebase Notification',
-              body: `HI Dear ${user.displayName}!`,
-            },
-            token: pushId
-          };
-          // sendToDevice can also accept an array of push tokens
-          return admin.messaging().send(message).then(() => {
-            console.log('Notification has been sent!!!')
-          }).catch(e => {
-            console.log('Notification hasn\'t been sent!!! ', e)
-          });
-        }).catch((e) => {
-          console.log('Something went wrong!!!', e);
-        });
-      }
+    const uid = context.params.uid;
+    const pushId = change.after.val();
 
-      return Promise.resolve(false);
+    return admin.database().ref('users/' + uid).once('value', snap => {
+      const user = snap.val();
+      console.log('User ', user);
+      console.log('Push Token: ', pushId);
+      //Click the following link: https://rntrainingtest.page.link/welcome
+      const message = {
+        notification: {
+          title: 'Firebase Auth Notification',
+          body: `Dear ${user.displayName}! Thanks for registration to react native courses.`,
+        },
+        token: pushId
+      };
+      // sendToDevice can also accept an array of push tokens
+      return admin.messaging().send(message).then(() => {
+        console.log('Notification has been sent!!!')
+      }).catch(e => {
+        console.log('Notification hasn\'t been sent!!! ', e)
+      });
+    }).catch((e) => {
+      console.log('Something went wrong!!!', e);
+    });
   });
