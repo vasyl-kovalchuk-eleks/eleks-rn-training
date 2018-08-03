86837e3390a86f19b4a716f6d92061051b81969d
Vasyl Kovalchuk <v.kovalchuk@eleks.com>
2018-08-03 19:50:11 +0300

    fix index file

--- packages/topic2/App.js
+++ packages/topic2/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import logger from "redux-logger"
 import thunk from "redux-thunk"
 import AppReducer from './src/reducers';
@@ -18,7 +17,5 @@ class ReduxExampleApp extends React.Component {
     );
   }
 }
-
-AppRegistry.registerComponent('ReduxExample', () => ReduxExampleApp);
 
 export default ReduxExampleApp;
